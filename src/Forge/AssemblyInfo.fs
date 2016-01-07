772405be3cea26fc24b23e98eb83d7fbc577388c
Jared Hester <jared.hester@gmail.com>
2016-01-07 03:42:31 -0500

    setup project

---
+++ src/Forge/AssemblyInfo.fs
@@ -0,0 +1,12 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("Forge")>]
+[<assembly: AssemblyProductAttribute("Forge")>]
+[<assembly: AssemblyDescriptionAttribute("F# tool for proj and sln generation")>]
+[<assembly: AssemblyVersionAttribute("1.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "1.0"
